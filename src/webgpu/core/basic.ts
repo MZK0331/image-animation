65d7b10a1f0d9c8c0b36923c5a1f29662f3b422e
mzk <mizuki.20030331@gmail.com>
2026-01-02 00:39:00 +0900

    update

--- src/webgpu/core/basic.ts
+++ src/webgpu/core/basic.ts
@@ -1,10 +1,12 @@
 import { subscribeMouseMoveListener } from "../../events/mousemove"
 import { subscribeResizeListener } from "../../events/resize"
+import { subscribeTouchMoveListener } from "../../events/touchmove"
 
 export class Basic {
     constructor() {
         subscribeResizeListener(this.onResize.bind(this))
         subscribeMouseMoveListener(this.onMouseMove.bind(this))
+        subscribeTouchMoveListener(this.onTouchMove.bind(this))
     }
 
     public startRendering() {
